d28ff028e97bab4f8e335f07955cf277388e5345
kristivent <ventk84@gmail.com>
2024-10-26 13:34:40 -0400

    error message in CLI resolved

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -183,8 +183,8 @@ class Cli {
           parseInt(answers.year),
           parseInt(answers.weight),
           parseInt(answers.topSpeed),
+          [],
           parseInt(answers.towingCapacity),
-          []
         );
         // TODO: push the truck to the vehicles array
         this.vehicles.push(truck);
